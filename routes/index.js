80f807161acae145b6a5f2dffe4cda403b8f8007
paralainer <serg.talov@gmail.com>
2015-01-06 00:51:52 +0400

    updated libs

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,6 @@ exports.index = function (req, res) {
 exports.client = function (req, res) {
     res.render('client/index');
 };
-exports.client2 = function (req, res) {
-    res.render('client/index2');
-};
 
 exports.commandCenter = function (req, res) {
     res.render('commandcenter/index');
